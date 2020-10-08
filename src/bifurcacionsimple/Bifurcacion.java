c58f641365e5e149970e8978831861a8f6d067bd
hermann-505 <>
2020-10-08 10:50:22 -0600

    Ejemplos de bifurcación if..else

---
+++ src/bifurcacionsimple/Bifurcacion.java
@@ -0,0 +1,49 @@
+package bifurcacionsimple;
+
+public class Bifurcacion {
+    private int entero;
+    private float decimal;
+    private boolean logico;
+    private char caracter;
+    private String cadena;
+
+    public int getEntero() {
+        return entero;
+    }
+
+    public void setEntero(int entero) {
+        this.entero = entero;
+    }
+
+    public float getDecimal() {
+        return decimal;
+    }
+
+    public void setDecimal(float decimal) {
+        this.decimal = decimal;
+    }
+
+    public boolean getLogico() {
+        return logico;
+    }
+
+    public void setLogico(boolean logico) {
+        this.logico = logico;
+    }
+
+    public char getCaracter() {
+        return caracter;
+    }
+
+    public void setCaracter(char caracter) {
+        this.caracter = caracter;
+    }
+
+    public String getCadena() {
+        return cadena;
+    }
+
+    public void setCadena(String cadena) {
+        this.cadena = cadena;
+    }
+}
---
+++ src/bifurcacionsimple/BifurcacionSimple.java
@@ -0,0 +1,10 @@
+package bifurcacionsimple;
+
+public class BifurcacionSimple {
+    
+    public static void main(String[] args) {
+        Ventana win = new Ventana();
+        win.setVisible(true);
+    }
+    
+}
